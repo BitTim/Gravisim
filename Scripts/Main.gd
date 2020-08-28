5666a475b6426a73704af1d2c281662e6c2e5235
BitTim <tim.anhalt.2004@protonmail.com>
2020-08-28 23:19:13 +0200

    Added Color and fixed Algorithm

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -15,15 +15,18 @@ var objScene = preload("res://Scenes/Object.tscn")
 func _ready():
 	rng.randomize()
 	
-	#for _i in range(0, numObjects):
-	#	var obj = objScene.instance()
-	#	obj.mass = rng.randi_range(1, 999)
-	#	
-	#	obj.position = Vector2(rng.randf_range(0.0, 640.0), rng.randf_range(0.0, 480.0))
-	#	$Objects.add_child(obj)
-	
-	objs = $Objects.get_children()
+	for _i in range(0, numObjects):
+		var obj = objScene.instance()
+		obj.mass = rng.randi_range(1, 9999)
+		
+	#	obj.dir = Vector2(rng.randf_range(-1, 1), rng.randf_range(-1, 1))
+	#	obj.speed = rng.randi_range(0, 100)
+		
+		obj.position = Vector2(rng.randf_range(0.0, 1280.0), rng.randf_range(0.0, 720.0))
+		$Objects.add_child(obj)
 
 func _physics_process(delta):
+	objs = $Objects.get_children()
+	
 	for o in objs:
 		o.applyGravity(objs, delta)
--- Scripts/Object.gd
+++ Scripts/Object.gd
@@ -10,23 +10,102 @@ export (bool) var immovable = false
 export (Vector2) var dir
 export (int) var speed
 
-const G = 667.4
-const LPT = 512
+const G = 6674
+const LPT = 2048
 
 var radius
 var vel = Vector2()
 var accel = Vector2()
 
+var col = Color()
+
+var rng = RandomNumberGenerator.new()
+
+# ================================
+# Utility
+# ================================
+
 func _ready():
-	radius = sqrt(mass)
+	rng.randomize()
+	
+	radius = getRadius()
 	vel = dir * speed
 	
 	var shape = CircleShape2D.new()
 	shape.set_radius(radius)
 	$CollisionShape2D.shape = shape
+	
+	col = Color(rng.randf_range(0.5, 1), rng.randf_range(0.5, 1), rng.randf_range(0.5, 1), 1)
+	$CanvasLayer/Line2D.default_color= col
 
 func _draw():
-	draw_circle(Vector2(), radius, Color(1, 1, 1, 1))
+	draw_circle(Vector2(), radius, col)
+
+func getRadius():
+	return round(sqrt(mass / 10.0))
+
+func updateValues(mass):
+	self.mass = mass
+	radius = getRadius()
+	
+	var shape = CircleShape2D.new()
+	shape.set_radius(radius)
+	$CollisionShape2D.shape = shape
+	
+	update()
+
+func updateLine():
+	if $CanvasLayer/Line2D.get_point_count() > LPT:
+		$CanvasLayer/Line2D.remove_point(0);
+	
+	if $CanvasLayer/Line2D.get_point_count() > 0:
+		if $CanvasLayer/Line2D.get_point_position($CanvasLayer/Line2D.get_point_count() - 1) != global_position:
+			$CanvasLayer/Line2D.add_point(global_position)
+	else:
+		$CanvasLayer/Line2D.add_point(global_position)
+
+# ================================
+# Movement
+# ================================
+
+func move(delta):
+	vel += accel * delta
+	
+	var collision = move_and_collide(vel * delta)
+	if collision != null:
+		collide(collision.collider)
+	
+	if (position.y + radius >= 720):
+		vel.y = -vel.y
+		position.y = 719 - radius
+	if (position.y - radius < 0):
+		vel.y = -vel.y
+		position.y = radius
+	if (position.x + radius >= 1280):
+		vel.x = -vel.x
+		position.x = 1279 - radius
+	if (position.x - radius < 0):
+		vel.x = -vel.x
+		position.x = radius
+	
+	updateLine()
+
+func collide(obj):
+	if obj.mass > mass:
+		obj.updateValues(obj.mass + mass)
+		queue_free()
+		return
+	
+	if obj.mass == mass:
+		if obj.get_instance_id() > get_instance_id():
+			obj.updateValues(obj.mass + mass)
+			get_parent().remove_child(self)
+			queue_free()
+			return
+
+# ================================
+# Gravity
+# ================================
 
 func applyGravity(objects, delta):
 	if immovable:
@@ -44,16 +123,5 @@ func applyGravity(objects, delta):
 		
 		force += F
 	
-	accel = force / mass
-	
-	vel += accel * delta
-	vel = move_and_slide(vel)
-	
-	if $CanvasLayer/Line2D.get_point_count() > LPT:
-		$CanvasLayer/Line2D.remove_point(0);
-	
-	if $CanvasLayer/Line2D.get_point_count() > 0:
-		if $CanvasLayer/Line2D.get_point_position($CanvasLayer/Line2D.get_point_count() - 1) != global_position:
-			$CanvasLayer/Line2D.add_point(global_position)
-	else:
-		$CanvasLayer/Line2D.add_point(global_position)
+	accel = force / pow(mass, 2)
+	move(delta)
