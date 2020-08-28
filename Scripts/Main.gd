40b04feb421f4631d0ebb38adc75e1eec5e96a38
BitTim <tim.anhalt.2004@protonmail.com>
2020-08-28 09:03:57 +0200

    Added Trailing Lines

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -6,10 +6,23 @@
 
 extends Node2D
 
-onready var objs = $Objects.get_children()
+export (int) var numObjects
+
+var objs = null
+var rng = RandomNumberGenerator.new()
+var objScene = preload("res://Scenes/Object.tscn")
 
 func _ready():
-	pass
+	rng.randomize()
+	
+	#for _i in range(0, numObjects):
+	#	var obj = objScene.instance()
+	#	obj.mass = rng.randi_range(1, 999)
+	#	
+	#	obj.position = Vector2(rng.randf_range(0.0, 640.0), rng.randf_range(0.0, 480.0))
+	#	$Objects.add_child(obj)
+	
+	objs = $Objects.get_children()
 
 func _physics_process(delta):
 	for o in objs:
--- Scripts/Object.gd
+++ Scripts/Object.gd
@@ -5,24 +5,33 @@
 
 extends KinematicBody2D
 
-export (int) var radius
 export (int) var mass
+export (bool) var immovable = false
+export (Vector2) var dir
+export (int) var speed
 
-const G = 6674
+const G = 667.4
+const LPT = 512
 
+var radius
 var vel = Vector2()
 var accel = Vector2()
 
 func _ready():
-  var shape = CircleShape2D.new()
-  shape.set_radius(radius)
-  
-  $CollisionShape2D.shape = shape
+	radius = sqrt(mass)
+	vel = dir * speed
+	
+	var shape = CircleShape2D.new()
+	shape.set_radius(radius)
+	$CollisionShape2D.shape = shape
 
 func _draw():
-  draw_circle(Vector2(), radius, Color(1, 1, 1, 1))
+	draw_circle(Vector2(), radius, Color(1, 1, 1, 1))
 
 func applyGravity(objects, delta):
+	if immovable:
+		return
+	
 	var force = Vector2()
 	
 	for o in objects:
@@ -39,3 +48,12 @@ func applyGravity(objects, delta):
 	
 	vel += accel * delta
 	vel = move_and_slide(vel)
+	
+	if $CanvasLayer/Line2D.get_point_count() > LPT:
+		$CanvasLayer/Line2D.remove_point(0);
+	
+	if $CanvasLayer/Line2D.get_point_count() > 0:
+		if $CanvasLayer/Line2D.get_point_position($CanvasLayer/Line2D.get_point_count() - 1) != global_position:
+			$CanvasLayer/Line2D.add_point(global_position)
+	else:
+		$CanvasLayer/Line2D.add_point(global_position)
